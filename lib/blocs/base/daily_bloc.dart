d0b18c0ad1fd1aa5f49a2ce0e3901c9bb6018bc4
Jeanie <whdpwls63@gmail.com>
2024-02-02 17:36:17 +0900

    daily page mockup

--- lib/blocs/base/daily_bloc.dart
+++ lib/blocs/base/daily_bloc.dart
@@ -1,8 +1,36 @@
 import 'package:habit_app/utils/disposable.dart';
+import 'package:rxdart/rxdart.dart';
+
+const int prevDates = 30;
 
 class DailyBloc extends Disposable {
-  DailyBloc();
+  late final List<DateTime> dates;
+
+  final BehaviorSubject<int> _dateIndex = BehaviorSubject.seeded(prevDates);
+  Stream<int> get dateIndex => _dateIndex.stream;
+  Function get setDateIndex => _dateIndex.add;
+
+  DailyBloc() {
+    dates = getDates();
+  }
 
   @override
-  void dispose() {}
+  void dispose() {
+    _dateIndex.close();
+  }
+
+  List<DateTime> getDates() {
+    DateTime today = DateTime.now();
+    DateTime lastMonth = today.subtract(const Duration(days: prevDates));
+
+    List<DateTime> items = List<DateTime>.generate(
+        60,
+        (i) => DateTime.utc(
+              lastMonth.year,
+              lastMonth.month,
+              lastMonth.day,
+            ).add(Duration(days: i)));
+
+    return items;
+  }
 }
--- lib/pages/base/daily_page.dart
+++ lib/pages/base/daily_page.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:habit_app/blocs/base/daily_bloc.dart';
 import 'package:habit_app/styles/colors.dart';
+import 'package:habit_app/styles/tokens.dart';
 import 'package:habit_app/styles/typos.dart';
+import 'package:habit_app/utils/functions.dart';
 import 'package:habit_app/widgets/ht_text.dart';
+import 'package:provider/provider.dart';
 
 class DailyPage extends StatelessWidget {
   const DailyPage({super.key});
@@ -11,6 +15,7 @@ class DailyPage extends StatelessWidget {
     return const Column(
       children: [
         DailyAppbar(),
+        DailyDates(),
         DailyTaskList(),
       ],
     );
@@ -24,13 +29,95 @@ class DailyAppbar extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Column(
       children: [
-        HTText(
-          'Task',
-          typoToken: HTTypoToken.headlineLarge,
-          color: HTColors.gray080,
+        Padding(
+          padding: HTEdgeInsets.h24v16,
+          child: Row(
+            children: [
+              HTText(
+                'Task',
+                typoToken: HTTypoToken.headlineLarge,
+                color: HTColors.black,
+              ),
+              Spacer(),
+              InkWell(
+                child: Icon(
+                  Icons.menu_rounded,
+                  size: 24,
+                ),
+              )
+            ],
+          ),
         )
       ],
     );
+  }
+}
+
+class DailyDates extends StatelessWidget {
+  const DailyDates({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    DailyBloc dailyBloc = context.read<DailyBloc>();
+
+    ScrollController dateScrollController = ScrollController(
+        initialScrollOffset: ((52 + 8) * prevDates -
+                ((MediaQuery.sizeOf(context).width - 86) / 2))
+            .toDouble());
+
+    return StreamBuilder<int>(
+        stream: dailyBloc.dateIndex,
+        builder: (context, snapshot) {
+          int dateIndex = snapshot.data ?? 0;
+
+          return SizedBox(
+            height: 52 + 16,
+            child: ListView.separated(
+                controller: dateScrollController,
+                scrollDirection: Axis.horizontal,
+                padding: HTEdgeInsets.horizontal16,
+                shrinkWrap: true,
+                itemBuilder: (context, index) {
+                  DateTime date = dailyBloc.dates[index];
+                  bool isSelected = index == dateIndex;
+
+                  return GestureDetector(
+                    onTap: () {
+                      dailyBloc.setDateIndex(index);
+                    },
+                    child: Container(
+                      width: 52,
+                      height: 52,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: isSelected ? HTColors.black : HTColors.white,
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          HTText(
+                            weekdayToText(date.weekday),
+                            typoToken: HTTypoToken.captionXSmall,
+                            color: HTColors.gray030,
+                          ),
+                          HTSpacers.height1,
+                          HTText(
+                            date.day.toString(),
+                            typoToken: HTTypoToken.subtitleLarge,
+                            color: isSelected ? HTColors.white : HTColors.black,
+                            height: 1,
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+                separatorBuilder: (context, index) {
+                  return HTSpacers.width8;
+                },
+                itemCount: dailyBloc.dates.length),
+          );
+        });
   }
 }
 
@@ -39,8 +126,85 @@ class DailyTaskList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Expanded(
-      child: SingleChildScrollView(),
+    return Expanded(
+      child: Container(
+          color: HTColors.gray010,
+          child: SingleChildScrollView(
+            padding: HTEdgeInsets.all24,
+            child: Column(
+              children: [
+                const Row(
+                  children: [
+                    HTText(
+                      '4 Tasks',
+                      typoToken: HTTypoToken.captionMedium,
+                      color: HTColors.gray040,
+                    ),
+                    Spacer(),
+                    HTText(
+                      '2 Done',
+                      typoToken: HTTypoToken.captionMedium,
+                      color: HTColors.gray040,
+                    ),
+                  ],
+                ),
+                HTSpacers.height8,
+                ListView.separated(
+                    shrinkWrap: true,
+                    physics: const NeverScrollableScrollPhysics(),
+                    itemBuilder: (context, index) {
+                      return Container(
+                        width: double.infinity,
+                        padding: HTEdgeInsets.h24v16,
+                        decoration: BoxDecoration(
+                            color: HTColors.white,
+                            borderRadius: HTBorderRadius.circular12),
+                        child: const Row(
+                          children: [
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                HTText(
+                                  '📕',
+                                  typoToken: HTTypoToken.captionLarge,
+                                  color: HTColors.black,
+                                ),
+                                HTSpacers.height4,
+                                HTText(
+                                  'English Study',
+                                  typoToken: HTTypoToken.headlineSmall,
+                                  color: HTColors.black,
+                                ),
+                                HTSpacers.height4,
+                                HTText(
+                                  'until 24.01.26 ㆍ Weekday',
+                                  typoToken: HTTypoToken.captionSmall,
+                                  color: HTColors.gray040,
+                                ),
+                              ],
+                            ),
+                            Spacer(),
+                            Column(
+                              children: [
+                                Icon(Icons.check_circle),
+                                HTText(
+                                  'Done',
+                                  typoToken: HTTypoToken.headlineXXSmall,
+                                  color: HTColors.black,
+                                )
+                              ],
+                            )
+                          ],
+                        ),
+                      );
+                    },
+                    separatorBuilder: (context, index) {
+                      return HTSpacers.height8;
+                    },
+                    itemCount: 10)
+              ],
+            ),
+          )),
     );
   }
 }
