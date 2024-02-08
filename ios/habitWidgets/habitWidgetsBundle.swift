e5c31873d08192d2d49ba851a05afd6a047f3c49
Jeanie <whdpwls63@gmail.com>
2024-02-09 01:59:01 +0900

    example widget setup

---
+++ ios/habitWidgets/habitWidgetsBundle.swift
@@ -0,0 +1,16 @@
+//
+//  habitWidgetsBundle.swift
+//  habitWidgets
+//
+//  Created by Jeanie Cho on 2/9/24.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct habitWidgetsBundle: WidgetBundle {
+    var body: some Widget {
+        habitWidgets()
+    }
+}
