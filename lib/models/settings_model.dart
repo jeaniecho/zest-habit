865cbc166a7d121b8cffe2ad26ca57f9b09b4438
Jeanie <whdpwls63@gmail.com>
2024-03-17 12:42:09 +0900

    onboarding setup

--- lib/models/settings_model.dart
+++ lib/models/settings_model.dart
@@ -8,24 +8,28 @@ class Settings {
   bool isDarkMode;
   bool isNotificationOn;
   int createdTaskCount;
+  bool completedOnboarding;
 
   Settings({
     this.id = 1,
     this.isDarkMode = false,
     this.isNotificationOn = false,
     this.createdTaskCount = 0,
+    this.completedOnboarding = false,
   });
 
   Settings copyWith({
     bool? isDarkMode,
     bool? isNotificationOn,
     int? createdTaskCount,
+    bool? completedOnboarding,
   }) {
     return Settings(
       id: 1,
       isDarkMode: isDarkMode ?? this.isDarkMode,
       isNotificationOn: isNotificationOn ?? this.isNotificationOn,
       createdTaskCount: createdTaskCount ?? this.createdTaskCount,
+      completedOnboarding: completedOnboarding ?? this.completedOnboarding,
     );
   }
 }
